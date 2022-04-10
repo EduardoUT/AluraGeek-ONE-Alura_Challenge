0f8a6ba467e15e23cfba893c64cbe7cbc3aaffd3
Eduardo <eduardoreyeshernandez98@gmail.com>
2022-04-09 20:55:52 -0500

    Agregando función para buscador en versión móvil

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,18 @@
 import mostrarOcultarMenu from "./componentes/mostrarOcultarMenu.js";
+import mostrarOcultarBuscador from "./componentes/buscadorMovil.js";
 
 /**Función IIFE */
 (() => {
+    const mediaQuery = window.matchMedia("(max-width: 767px)");
+    const botonBuscarMovil = document.querySelector(".cabecera__botonBuscarMovil");
+    const logoCabecera = document.querySelector(".cabecera__logo");
+    const botonLogin = document.querySelector(".cabecera__boton");
+    const formularioBusqueda = document.querySelector(".cabecera__formulario");
+    const campoBuscar = document.querySelector(".cabecera__campo");
+    const containerLogoBuscador = document.querySelector(".cabecera__logoBuscador");
     mostrarOcultarMenu();
+    mostrarOcultarBuscador(mediaQuery, botonBuscarMovil, logoCabecera,
+        botonLogin, formularioBusqueda, campoBuscar, containerLogoBuscador);
 })();
 
 window.addEventListener("load", (event) => {
