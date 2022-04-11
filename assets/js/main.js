05ca00d633aac894bfbc55c675c9555756cb765a
Eduardo <eduardoreyeshernandez98@gmail.com>
2022-04-11 15:50:11 -0500

    Reorganizando código agregando nuevas funciones y encapsulando eventos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 import mostrarOcultarMenu from "./componentes/mostrarOcultarMenu.js";
 import mostrarOcultarBuscador from "./componentes/buscadorMovil.js";
+import { valida } from "./formularios/validaciones.js";
+import habilitarBtnRodapie from "./componentes/habilitarBoton.js";
 
 /**Función IIFE */
 (() => {
@@ -9,10 +11,39 @@ import mostrarOcultarBuscador from "./componentes/buscadorMovil.js";
     const campoBuscar = document.querySelector(".cabecera__campo");
     const botonLogin = document.querySelector(".cabecera__boton");
     const botonBuscarMovil = document.querySelector(".cabecera__botonBuscarMovil");
+    const campos = document.querySelectorAll("[data-contacto]");
+    const btnEnviar = document.getElementById("btn-enviar");
+    const formularioRodapie = document.getElementById("formulario-rodapie");
+
     mostrarOcultarMenu();
     mostrarOcultarBuscador(logoBuscadorContainer, logoCabecera,
         formularioBusqueda, campoBuscar, botonBuscarMovil, botonLogin);
-    window.addEventListener("load", (event) => {
+    habilitarBtnRodapie(btnEnviar);
+
+    const validarCampos = (campos) => {
+        campos.forEach((campo) => {
+            campo.addEventListener("blur", (campo) => {
+                valida(campo.target);
+                habilitarBtnRodapie(btnEnviar);
+            });
+        });
+    }
+
+    const validarBtnRodapie = (event) => {
+        const element = event.target;
+        if (element && element.tagName == 'INPUT') {
+            habilitarBtnRodapie(btnEnviar);
+        } else if (element && element.tagName == 'TEXTAREA') {
+            habilitarBtnRodapie(btnEnviar);
+        }
+    }
+
+    const evitarRecarga = (event) => {
         event.preventDefault();
-    });
+    }
+
+    window.addEventListener("load", evitarRecarga);
+    btnEnviar.addEventListener("click", evitarRecarga);
+    formularioRodapie.addEventListener("keyup", validarBtnRodapie);
+    validarCampos(campos);
 })();
