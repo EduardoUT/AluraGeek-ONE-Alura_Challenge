e07f37fcf12f2e4fa78105a3f41ce872d8f68148
Eduardo <eduardoreyeshernandez98@gmail.com>
2022-04-18 16:07:45 -0500

    Agregando referencia web

--- assets/js/componentes/cabecera.js
+++ assets/js/componentes/cabecera.js
@@ -1,3 +1,7 @@
+/**
+ * Referencia:
+ * https://www.freecodecamp.org/news/reusable-html-components-how-to-reuse-a-header-and-footer-on-a-website/
+ */
 class Header extends HTMLElement {
     constructor() {
         super();
