4fbbc2813ea5b3f0f747eea79e60fda29582a815
Eduardo <eduardoreyeshernandez98@gmail.com>
2022-04-19 12:30:50 -0500

    Actualizando nombre de función y comentando

--- assets/js/componentes/mediaQuery.js
+++ assets/js/componentes/mediaQuery.js
@@ -3,7 +3,7 @@
  * que permitan modificar el contenido de la página que no sean
  * posibles de modificar con CSS p.ej.
  * 
- * if (mediaQueryCelular) {código a ejecutar para celulares} else {código a ejecutarse fuera del rango}
+ * if (mediaQueryCelular()) {código a ejecutar para celulares} else {código a ejecutarse fuera del rango}
  * @returns {boolean} Si el resultado es true, el contenido cambiara para
  * dispositivos móviles entre 0px a 767px.
  */
