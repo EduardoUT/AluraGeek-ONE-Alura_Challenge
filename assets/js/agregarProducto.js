3b9ad2e4adacfd7c53525f02b797207e3ee94950
Eduardo <eduardoreyeshernandez98@gmail.com>
2022-04-19 16:24:04 -0500

    Agregando validación en precio

--- assets/js/agregarProducto.js
+++ assets/js/agregarProducto.js
@@ -1,5 +1,19 @@
 import dragAndDropBox from "./formularios/arrastrarSoltarImagen.js";
 
 (() => {
+    const precio = document.getElementById("precioProducto");
     dragAndDropBox();
+    const checkPrecio = (event) => {
+        let valorIngresado = event.target.value;
+        let valorFinal = "";
+        for (let i = 0; i <= valorIngresado.length; i++) {
+            if (valorIngresado[0] == 0) {
+                valorFinal += "";
+            } else {
+                valorFinal = valorIngresado;
+            }
+        }
+        precio.value = valorFinal;
+    }
+    precio.addEventListener("keyup", checkPrecio);
 })();
