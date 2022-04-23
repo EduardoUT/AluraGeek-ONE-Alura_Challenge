ed699617e7ef1722fbcaca996301a9fdae698393
Eduardo <eduardoreyeshernandez98@gmail.com>
2022-04-23 17:56:20 -0500

    Agregando validación para precio

--- assets/js/agregarProducto.js
+++ assets/js/agregarProducto.js
@@ -1,20 +1,22 @@
 const precio = document.getElementById("precioProducto");
 const btnEnviar = document.getElementById("agregarProducto");
+
 /**
  * Función asociada a evento keyup del campo precio,
- * al escribir evita que el usuario ingrese primero un 0.
+ * al escribir evita que el usuario ingrese cifras con precios 0.
  * @param {keyup} event 
  */
- const checkPrecio = (event) => {
+const validarPrecioIngresado = (event) => {
     let valorIngresado = event.target.value;
     let valorFinal = "";
-    for (let i = 0; i <= valorIngresado.length; i++) {
-        if (valorIngresado[0] == 0) {
-            valorFinal += "";
-        } else {
-            valorFinal = valorIngresado;
-        }
+    const cifrasInvalidas = ["0000000.00", "000000.00", "00000.00", "0000.00", "000.00", "00.00", "0.00"];
+    const esInvalido = cifrasInvalidas.includes(valorIngresado);
+    if (esInvalido) {
+        valorFinal += "";
+    } else {
+        valorFinal = valorIngresado;
     }
     precio.value = valorFinal;
 }
-precio.addEventListener("keyup", checkPrecio);
+
+precio.addEventListener("keyup", validarPrecioIngresado);
