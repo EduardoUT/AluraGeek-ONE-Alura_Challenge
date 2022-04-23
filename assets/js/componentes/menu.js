720a2776a78c608ca4354a7efcf3701933bd99b3
Eduardo <eduardoreyeshernandez98@gmail.com>
2022-04-23 14:35:47 -0500

    Modificando código

--- assets/js/componentes/menu.js
+++ assets/js/componentes/menu.js
@@ -1,78 +1,132 @@
-/**Encapsulando función */
+import { mediaQueryCelularHorizontal } from "../componentes/mediaQuery.js";
+
+/**
+ * @const
+ * Selecciona el elemento body del sitio web.
+ */
+const body = document.body;
+
+/**
+ * @const
+ * Asigna el nombre de una clase css titulada scroll-up.
+ * 
+ * Esta clase css se ubica en headerPrincipal.css
+ */
+const scrollArriba = "scroll-up";
+/**
+ * @const
+ * Asigna el nombre de una clase css titula scroll-down.
+ * 
+ * Esta clase css se ubica en headerPrincipal.css
+*/
+const scrollAbajo = "scroll-down";
+/**
+ * @const 
+ * Almacena la última posición de desplazamiento.
+ */
+let scrollUltimo = 0;
+
+/**
+ * Contiene funciones que modifican la visualización del menú
+ * al hacer scroll o al llenar el formulario en posición
+ * horiontal en dispositivos móviles.
+ */
 const menu = () => {
-    /**
-     * @const
-     * Selecciona el elemento body del sitio web.
-    */
-    const body = document.body;
+    visibilidadMenuEnFormulario();
+    window.addEventListener("resize", visibilidadMenuEnFormulario);
+    window.addEventListener("scroll", visualizacionMenu);
+}
 
-    /**
-     * @const
-     * Asigna el nombre de una clase css titulada scroll-up.
+/**
+     * @param {input} inputs
+     * Elementos input del DOM en el formulario. 
+     * @param {textarea} textArea 
+     * Elemento textarea del DOM en el formulario.
+     * @returns void.
+     * @function mediaQueryCelularHorizontal() Función que devuelve un valor boleano 
+     * en caso de cumplirse el media query, para valores entre min-width y max-width.
      * 
-     * Esta clase css se ubica en headerPrincipal.css
+     * La condición permite validar un mediaQuery con un rango min-width y max-width.
+     * Esto a fin de evitar abarcar toda la pantalla mientras el teclado en Android, iOS, etc,
+     * se encuentra abierto en dispositivos móviles orientados horizontalmente.
+     *
+     * Eventos para @param inputs
+     * focus: 
+     * 
+     * Cuando un campo contenga el foco, el menú se ocultará. 
+     * 
+     * keyup: 
+     * 
+     * Cuando el usuario escriba en el campo input, el menú se ocultará.
+     * 
+     * blur:
+     * 
+     * Cuando el campo pierda el foco, el menú volerá a visualizarse.
      */
-    const scrollArriba = "scroll-up";
+const visibilidadMenuEnFormulario = () => {
+    const inputs = document.querySelectorAll("[data-campo]");
+    const anchoValido = mediaQueryCelularHorizontal();
+    if (anchoValido) {
+        inputs.forEach((input) => {
+            input.addEventListener("focus", ocultarMenu);
+            input.addEventListener("keyup", ocultarMenu);
+            input.addEventListener("blur", mostrarMenu);
+        });
+    } else {
+        inputs.forEach((input) => {
+            input.removeEventListener("focus", ocultarMenu);
+            input.removeEventListener("keyup", ocultarMenu);
+            input.removeEventListener("blur", mostrarMenu);
+        });
+    }
+    return;
+}
 
+const visualizacionMenu = () => {
     /**
      * @const
-     * Asigna el nombre de una clase css titula scroll-down.
-     * 
-     * Esta clase css se ubica en headerPrincipal.css
-    */
-    const scrollAbajo = "scroll-down";
-
-    /**
-     * @const 
-     * Almacena la última posición de desplazamiento.
+     * Detecta la posición actual de scroll, por medio
+     * de window.scrollY.
      */
-    let scrollUltimo = 0;
-
-    const visualizacionMenu = () => {
-        /**
-         * @const
-         * Detecta la posición actual de scroll, por medio
-         * de window.scrollY.
-         */
-        const actualScroll = window.scrollY;
-        /**
-         * Si el valor es igual a cero, remueve la clase css
-         * scroll-up
-         */
-        if (actualScroll == 0) {
-            body.classList.remove(scrollArriba);
-            return;
-        }
-
-        /**
-         * Si el valor de actualScroll es mayor que scrollUltimo y 
-         * body no posee la clase scroll-down, removerá la clase
-         * scroll-arriba y agregará la clase scroll-down, de otra forma
-         * realizará el procedimiento contrario.
-         */
-        if (actualScroll > scrollUltimo && !body.classList.contains(scrollAbajo)) {
-            body.classList.remove(scrollArriba);
-            body.classList.add(scrollAbajo);
-        } else if (actualScroll < scrollUltimo && body.classList.contains(scrollAbajo)) {
-            body.classList.remove(scrollAbajo);
-            body.classList.add(scrollArriba);
-        }
-        /**
-         * Se asignará actualScroll a scrollUltimo
-         */
-        scrollUltimo = actualScroll;
+    const actualScroll = window.scrollY;
+    /**
+     * Si el valor es igual a cero, remueve la clase css
+     * scroll-up
+     */
+    if (actualScroll == 0) {
+        body.classList.remove(scrollArriba);
+        return;
     }
 
     /**
-     * Evento que detectará la posición del scroll
+     * Si el valor de actualScroll es mayor que scrollUltimo y 
+     * body no posee la clase scroll-down, removerá la clase
+     * scroll-arriba y agregará la clase scroll-down, de otra forma
+     * realizará el procedimiento contrario.
      */
-    window.addEventListener("scroll", visualizacionMenu);
-    /*Creditos:
-    George Martsoukos
-    Referencia:
-    https://webdesign.tutsplus.com/es/tutorials/how-to-hide-reveal-a-sticky-header-on-scroll-with-javascript--cms-33756
-    */
+    if (actualScroll > scrollUltimo && !body.classList.contains(scrollAbajo)) {
+        ocultarMenu();
+    } else if (actualScroll < scrollUltimo && body.classList.contains(scrollAbajo)) {
+        mostrarMenu();
+    }
+    /**
+     * Se asignará actualScroll a scrollUltimo
+     */
+    scrollUltimo = actualScroll;
+}
+/*Creditos:
+George Martsoukos
+https://webdesign.tutsplus.com/es/tutorials/how-to-hide-reveal-a-sticky-header-on-scroll-with-javascript--cms-33756
+*/
+
+const mostrarMenu = () => {
+    body.classList.add(scrollArriba);
+    body.classList.remove(scrollAbajo);
 }
 
-/**Exportando función */
+const ocultarMenu = () => {
+    body.classList.add(scrollAbajo);
+    body.classList.remove(scrollArriba);
+}
+
 export default menu;
