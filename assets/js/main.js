d480d606eb5329c7de6c444009b35502eba9594c
Eduardo <eduardoreyeshernandez98@gmail.com>
2022-04-23 15:39:10 -0500

    Eliminando selectores

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,18 +5,11 @@ import { habilitarBtnRodapie } from "./formularios/habilitarBoton.js";
 
 /**Función IIFE */
 (() => {
-    const logoBuscadorContainer = document.querySelector(".cabecera__logoBuscador");
-    const logoCabecera = document.querySelector(".cabecera__logo");
-    const formularioBusqueda = document.querySelector(".cabecera__formulario");
-    const campoBuscar = document.querySelector(".cabecera__campo");
-    const btnLoginCabecera = document.querySelector(".cabecera__boton");
-    const btnBuscarMovil = document.querySelector(".cabecera__botonBuscarMovil");
     const btnEnviar = document.getElementById("btn-enviar");
     const formularioRodapie = document.getElementById("formulario-rodapie");
 
     menu();
-    buscadorCelular(logoBuscadorContainer, logoCabecera,
-        formularioBusqueda, campoBuscar, btnBuscarMovil, btnLoginCabecera);
+    buscadorCelular();
     habilitarBtnRodapie(btnEnviar);
     validarCampos();
     
