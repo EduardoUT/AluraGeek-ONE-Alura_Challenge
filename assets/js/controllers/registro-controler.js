d373059378a8bffd2d4a947b1a05a02dea681ddc
Eduardo <eduardoreyeshernandez98@gmail.com>
2022-04-28 12:28:15 -0500

    Creando controlador para registro de productos

---
+++ assets/js/controllers/registro-controler.js
@@ -0,0 +1,23 @@
+import { precio } from "../formularios/agregarProducto.js";
+import { producto } from "../formularios/vistaPreviaImagen.js";
+
+const formAgregarProducto = document.querySelector("[data-form-add-product]");
+const nombreProducto = document.querySelector("[data-campo=producto]");
+const categoria = document.querySelector("[data-campo=categoria]");
+const descripcion = document.querySelector("[data-campo=descripcion]");
+
+const agregarProducto = (event) => {
+    event.preventDefault();
+    const valorImagen = producto.img;
+    const valorNombreProducto = nombreProducto.value;
+    const valorPrecio = precio.value;
+    const valorCategoria = categoria.value;
+    const valorDescripcion = descripcion.value;
+    console.log(valorImagen);
+    console.log(valorNombreProducto);
+    console.log(valorPrecio);
+    console.log(valorCategoria);
+    console.log(valorDescripcion);
+}
+
+formAgregarProducto.addEventListener("submit", agregarProducto);
