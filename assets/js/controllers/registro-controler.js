e5bc4885e8ad7f5d462a1d83e8e27805687b1f49
Eduardo <eduardoreyeshernandez98@gmail.com>
2022-04-28 19:46:51 -0500

    Agregando controlador de registro de productos

--- assets/js/controllers/registro-controler.js
+++ assets/js/controllers/registro-controler.js
@@ -1,10 +1,18 @@
 import { precio } from "../formularios/agregarProducto.js";
 import { producto } from "../formularios/imagenDropBoxArea.js";
+import { productServices } from "../service/product-service.js";
 
 const formAgregarProducto = document.querySelector("[data-form-add-product]");
 const nombreProducto = document.querySelector("[data-campo=producto]");
 const categoria = document.querySelector("[data-campo=categoria]");
 const descripcion = document.querySelector("[data-campo=descripcion]");
+
+const limpiarValores = () => {
+    producto.img = null;
+    nombreProducto.value = "";
+    precio.value = "";
+    descripcion.value = "";
+}
 
 const guardarProducto = (event) => {
     event.preventDefault();
@@ -13,11 +21,19 @@ const guardarProducto = (event) => {
     const valorPrecio = precio.value;
     const valorCategoria = categoria.value;
     const valorDescripcion = descripcion.value;
-    console.log(valorImagen);
-    console.log(valorNombreProducto);
-    console.log(valorPrecio);
-    console.log(valorCategoria);
-    console.log(valorDescripcion);
+    productServices.crearProducto(valorImagen, valorNombreProducto,
+        valorPrecio, valorCategoria, valorDescripcion)
+        .then((respuesta) => {
+            console.log(respuesta);
+            limpiarValores();
+            const redir = window.location.href = "../../../ventanas/guardado_exitosamente.html";
+            setTimeout(redir, 20000);
+
+        }).catch((error) => {
+            console.log(error)
+            const redir = window.location.href = "../../../ventanas/error.html";
+            setTimeout(redir, 20000);
+        });
 }
 
 formAgregarProducto.addEventListener("submit", guardarProducto);
