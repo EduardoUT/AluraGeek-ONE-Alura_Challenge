0b477f26a8ba6adb542a08f47f5cca779aec3e89
Eduardo <eduardoreyeshernandez98@gmail.com>
2022-04-29 19:46:18 -0500

    Organizando código y creando función para Cerrar Sesión

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,31 +2,52 @@ import menu from "./componentes/menu.js";
 import buscadorCelular from "./componentes/buscadorCelular.js";
 import validarCampos from "./formularios/validarCampos.js";
 import { habilitarBtnRodapie } from "./formularios/habilitarBoton.js";
+import { login } from "./login.js";
+sessionStorage.setItem("correo", "e@gmail.com");
+sessionStorage.setItem("password", "123");
 
-/**Función IIFE */
-(() => {
-    const btnEnviar = document.getElementById("btn-enviar");
-    const formularioRodapie = document.getElementById("formulario-rodapie");
+const btnLogin = document.querySelector("[data-btn-login]");
+const formularioRodapie = document.getElementById("formulario-rodapie");
+const btnEnviar = document.getElementById("btn-enviar");
+const estaAutenticado = JSON.parse(sessionStorage.getItem("autenticado"));
 
-    menu();
-    buscadorCelular();
-    habilitarBtnRodapie(btnEnviar);
-    validarCampos();
-    
-    const validarBtnRodapie = (event) => {
-        const element = event.target;
-        if (element && element.tagName == 'INPUT') {
-            habilitarBtnRodapie(btnEnviar);
-        } else if (element && element.tagName == 'TEXTAREA') {
-            habilitarBtnRodapie(btnEnviar);
-        }
+const botonLogin = () => {
+    const estaLogeado = JSON.parse(sessionStorage.getItem("autenticado"));
+    if (estaLogeado == null || estaLogeado == false) {
+        btnLogin.textContent = "Login";
+    } else {
+        btnLogin.textContent = "Salir";
     }
+}
 
-    const evitarRecarga = (event) => {
-        event.preventDefault();
+const clickOnLogin = () => {
+    if (btnLogin.textContent.includes("Login")) {
+        sessionStorage.setItem("autenticado", "false");
+    } else if (btnLogin.textContent.includes("Salir")) {
+        sessionStorage.setItem("autenticado", "false");
     }
+}
 
-    window.addEventListener("load", evitarRecarga);
-    btnEnviar.addEventListener("click", evitarRecarga);
-    formularioRodapie.addEventListener("keyup", validarBtnRodapie);
-})();
+const validarBtnRodapie = (event) => {
+    const element = event.target;
+    if (element && element.tagName == 'INPUT') {
+        habilitarBtnRodapie(btnEnviar);
+    } else if (element && element.tagName == 'TEXTAREA') {
+        habilitarBtnRodapie(btnEnviar);
+    }
+}
+
+const evitarRecarga = (event) => {
+    event.preventDefault();
+}
+
+login(estaAutenticado);
+botonLogin();
+menu();
+buscadorCelular();
+habilitarBtnRodapie(btnEnviar);
+validarCampos();
+window.addEventListener("load", evitarRecarga);
+btnLogin.addEventListener("click", clickOnLogin);
+btnEnviar.addEventListener("click", evitarRecarga);
+formularioRodapie.addEventListener("keyup", validarBtnRodapie);
