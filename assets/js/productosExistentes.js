a249859cf760e9b7701534a5a689ae42fa56da1c
Eduardo <eduardoreyeshernandez98@gmail.com>
2022-04-29 20:41:51 -0500

    Ocultando elementos CRUD dinámicamente

--- assets/js/productosExistentes.js
+++ assets/js/productosExistentes.js
@@ -1,4 +1,16 @@
 import { login } from "./login.js";
 import { estaAutenticado } from "./main.js";
 
-console.log(login(estaAutenticado));
+const btnIrAgregarProducto = document.querySelector("[data-agregar-producto]");
+const btnEliminarProducto = document.querySelector("[data-borrar-boton]");
+const btnEditarProducto = document.querySelector("[data-editar-boton]");
+
+const ocultarFuncionesCrud = () => {
+    if (!login(estaAutenticado)) {
+        btnIrAgregarProducto.style.display = "none";
+        btnEliminarProducto.style.display = "none";
+        btnEditarProducto.style.display = "none";
+    }
+}
+
+ocultarFuncionesCrud();
