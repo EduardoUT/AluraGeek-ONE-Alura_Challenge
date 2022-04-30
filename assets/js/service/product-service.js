77d2ade6c48f7c5239836755f6aa592ec327e682
Eduardo <eduardoreyeshernandez98@gmail.com>
2022-04-30 17:26:33 -0500

    Agregando método POST

--- assets/js/service/product-service.js
+++ assets/js/service/product-service.js
@@ -6,11 +6,36 @@ const crearProducto = (imagen, nombre, precio, categoria, desc) => {
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify({ id: uuid.v4(), imagen, nombre, precio, categoria, desc})
+        body: JSON.stringify({ id: uuid.v4(), imagen, nombre, precio, categoria, desc })
     });
-} 
+}
+
+const eliminarProducto = (id) => {
+    console.log("Eliminando el producto: " + id);
+    return fetch(`http://localhost:3000/producto/${id}`, {
+        method: "DELETE"
+    });
+}
+
+const actualizarProducto = async (id, imagen, nombre, precio, categoria, desc) => {
+    try {
+        const respuesta = await fetch(`http://localhost:3000/producto/${id}`,
+            {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ imagen, nombre, precio, categoria, desc })
+            });
+        return respuesta;
+    } catch (error) {
+        return console.log(error);
+    }
+}
 
 export const productServices = {
-    listaProductos,
     crearProducto,
+    listaProductos,
+    actualizarProducto,
+    eliminarProducto
 }
