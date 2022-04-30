d1a9439dad7959c02b982c4f06a9ebd72b9495c9
Eduardo <eduardoreyeshernandez98@gmail.com>
2022-04-30 15:02:46 -0500

    Eliminando alteración de estilos

--- assets/js/productosExistentes.js
+++ assets/js/productosExistentes.js
@@ -5,12 +5,10 @@ const btnIrAgregarProducto = document.querySelector("[data-agregar-producto]");
 const btnEliminarProducto = document.querySelector("[data-borrar-boton]");
 const btnEditarProducto = document.querySelector("[data-editar-boton]");
 
-const ocultarFuncionesCrud = () => {
+const ocultarBotonAddProduct = () => {
     if (!login(estaAutenticado)) {
         btnIrAgregarProducto.style.display = "none";
-        btnEliminarProducto.style.display = "none";
-        btnEditarProducto.style.display = "none";
     }
 }
 
-ocultarFuncionesCrud();
+ocultarBotonAddProduct();
