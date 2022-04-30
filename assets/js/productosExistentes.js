6db5b8658be6d9511c1c39499ff3803bcc6089c8
Eduardo <eduardoreyeshernandez98@gmail.com>
2022-04-30 15:03:29 -0500

    Eliminando selectores de botones

--- assets/js/productosExistentes.js
+++ assets/js/productosExistentes.js
@@ -2,8 +2,6 @@ import { login } from "./login.js";
 import { estaAutenticado } from "./main.js";
 
 const btnIrAgregarProducto = document.querySelector("[data-agregar-producto]");
-const btnEliminarProducto = document.querySelector("[data-borrar-boton]");
-const btnEditarProducto = document.querySelector("[data-editar-boton]");
 
 const ocultarBotonAddProduct = () => {
     if (!login(estaAutenticado)) {
