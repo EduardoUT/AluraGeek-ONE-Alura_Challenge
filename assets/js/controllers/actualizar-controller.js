378c26ef38f050a80ce9a80a0288b907becaa12b
Eduardo <eduardoreyeshernandez98@gmail.com>
2022-05-01 14:06:47 -0500

    Validando que se hicieron cambios en lo campos a actualizar

--- assets/js/controllers/actualizar-controller.js
+++ assets/js/controllers/actualizar-controller.js
@@ -1,5 +1,7 @@
 import { obtenerArchivoServer } from "../formularios/dropBoxArea.js";
-import { producto } from "../formularios/imagenDropBoxArea.js";
+import { habilitarBotonProducto, producto } from "../formularios/imagenDropBoxArea.js";
+import { login } from "../login.js";
+import { comprobarAcceso, estaAutenticado } from "../main.js";
 import { productServices } from "../service/product-service.js";
 
 const formulario = document.querySelector("[data-form-update-product]");
@@ -29,28 +31,63 @@ const obtenerInformacion = async () => {
             precio.value = productoDetalles.precio;
             categoria.value = productoDetalles.categoria;
             descripcion.value = productoDetalles.desc;
+            habilitarBotonProducto();
         } else {
             throw new Error();
         }
     } catch (error) {
         console.log(error);
         window.location.href = "/ventanas/error.html";
+
     }
 }
 
-formulario.addEventListener("submit", (event) => {
+formulario.addEventListener("submit", async (event) => {
     event.preventDefault();
     const url = new URL(window.location);
     const id = url.searchParams.get("id");
+    const productoServer = await productServices.detalleProducto(id);
     const imagen = producto.img;
     const nombre = document.querySelector("[data-campo=producto]").value;
     const precio = document.querySelector("[data-campo=precio]").value;
     const categoria = document.querySelector("[data-campo=categoria]").value;
     const descripcion = document.querySelector("[data-campo=descripcion]").value;
-    productServices.actualizarProducto(id, imagen, nombre, precio, categoria, descripcion)
-        .then(() => {
-            window.location.href = "/ventanas/actualizado_exitosamente.html";
-        });
+    const existeValorEnServidor = ((productoServer.imagen == imagen) && (productoServer.nombre == nombre) &&
+        (productoServer.precio == precio) && (productoServer.categoria == categoria) &&
+        (productoServer.desc == descripcion));
+
+    if (!existeValorEnServidor) {
+        Swal.fire({
+            icon: "question",
+            title: "¿Desea actualizar este producto?",
+            text: "Los cambios se verán reflejados inmediatamente.",
+            confirmTextButton: "Confirmar",
+            showCancelButton: true,
+            cancelButtonText: "Cancelar"
+        }).then((respuesta) => {
+            if (respuesta.isConfirmed) {
+                productServices.actualizarProducto(id, imagen, nombre, precio, categoria, descripcion)
+                    .then(() => {
+                        window.location.href = "/ventanas/actualizado_exitosamente.html";
+                    });
+            } else if (respuesta.isDismissed) {
+                Swal.fire({
+                    icon: "info",
+                    title: "Actualización cancelada."
+                }).then((respuesta) => {
+                    if (respuesta.isConfirmed) {
+                        window.location.reload();
+                    }
+                });
+            }
+        })
+    } else {
+        Swal.fire({
+            icon: "info",
+            title: "No se realizó ningún cambio.",
+            text: "Pruebe a editar algún campo para realizar una actualización.",
+        })
+    }
 });
 
 obtenerInformacion();
