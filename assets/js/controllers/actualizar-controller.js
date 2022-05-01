88096d98d8f11b3f6943669726ca5fa72b0c589f
Eduardo <eduardoreyeshernandez98@gmail.com>
2022-04-30 20:30:49 -0500

    Capturando valores através del id en el formulario

--- assets/js/controllers/actualizar-controller.js
+++ assets/js/controllers/actualizar-controller.js
@@ -1,3 +1,5 @@
+import { obtenerArchivoServer } from "../formularios/dropBoxArea.js";
+import { producto } from "../formularios/imagenDropBoxArea.js";
 import { productServices } from "../service/product-service.js";
 
 const formulario = document.querySelector("[data-form-update-product]");
@@ -9,18 +11,38 @@ const obtenerInformacion = async () => {
         window.location.href = "/ventanas/error.html";
     }
 
-    const dropBoxArea = document.querySelector("[data-dropbox-area]");
     const nombre = document.querySelector("[data-campo=producto]");
     const precio = document.querySelector("[data-campo=precio]");
     const categoria = document.querySelector("[data-campo=categoria]");
     const descripcion = document.querySelector("[data-campo=descripcion]");
-    console.log(dropBoxArea,nombre,precio,categoria,descripcion);
+    //console.log(dropBoxArea, nombre, precio, categoria, descripcion);
     try {
-        const producto = await productServices.detalleProducto(id);
-        console.log(producto);
+        const productoDetalles = await productServices.detalleProducto(id);
+        //console.log(productoDetalles);
+        //console.log(productoDetalles.imagen);
+        const existenValores = (productoDetalles.imagen && productoDetalles.nombre &&
+            productoDetalles.precio && productoDetalles.categoria && productoDetalles.desc);
+        if (existenValores) {
+            producto.img = productoDetalles.imagen;
+            obtenerArchivoServer(producto.img);
+            nombre.value = productoDetalles.nombre;
+            precio.value = productoDetalles.precio;
+            categoria.value = productoDetalles.categoria;
+            descripcion.value = productoDetalles.desc;
+        } else {
+            throw new Error();
+        }
     } catch (error) {
-        
+        console.log(error);
+        window.location.href = "/ventanas/error.html";
     }
 }
+
+formulario.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const url = new URL(window.location);
+    console.log(producto.img);
+
+});
 
 obtenerInformacion();
