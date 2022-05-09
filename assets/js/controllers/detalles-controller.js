db1c044f97621b0d3815c35e988bc887775f9def
Eduardo <eduardoreyeshernandez98@gmail.com>
2022-05-09 17:12:29 -0500

    Creando función para generar un producto aleatorio e ingresandolo a un array con items aleatorios

--- assets/js/controllers/detalles-controller.js
+++ assets/js/controllers/detalles-controller.js
@@ -5,14 +5,17 @@ const nombre = document.querySelector("[data-detalles-nombre]");
 const precio = document.querySelector("[data-detalles-precio]");
 const descripcion = document.querySelector("[data-detalles-dsc]");
 const titleWindow = document.querySelector("title");
+const seccionSimilares = document.querySelector("[data-productos-similares]");
+const listaDesordenada = [];
+
 
 const mostrarImagen = (imagen) => {
     const regexImagen = /^(([a-z\d]+)([-]?[a-z\d]+)+[.](jpeg|jpg|png))$/g;
     const esImagenLocal = regexImagen.test(imagen);
     if (esImagenLocal) {
-        imagenDetalles.setAttribute("style", `background: url('../assets/img/productos/${imagen}') center / 100% 100% no-repeat;`)
+        imagenDetalles.setAttribute("style", `background: url('../assets/img/productos/${imagen}') center / 100% 100% no-repeat;`);
     } else {
-        imagenDetalles.setAttribute("style", `background: url('${imagen}') center / 100% 100% no-repeat;`)
+        imagenDetalles.setAttribute("style", `background: url('${imagen}') center / 100% 100% no-repeat;`);
     }
 }
 
@@ -40,6 +43,72 @@ const obtenerDetallesProducto = async () => {
         console.log(error);
     }
 
-} 
+}
+
+const generarProductoAleatorio = (productos) => {
+    productos = productos.sort(() => { return Math.random() - 0.5 });
+    return productos;
+}
+
+const contenidoProductosLocales = (id, imagen, nombre, precio) => {
+    const contenido = `
+        <div class="productos__producto">
+            <div class="productos__imagen" style="background: url('./assets/img/productos/${imagen}') center / 100% 100% no-repeat;" tabindex="0"></div>
+            <p class="productos__nombre parrafo" tabindex="0">${nombre}</p>
+            <p class="productos__precio parrafo" tabindex="0">${precio}</p>
+            <a class="productos__link link" href="./ventanas/detalles.html?id=${id}" title="Ver más detalles" tabindex="0">Ver
+                Producto</a>
+        </div>
+    `;
+    return contenido;
+}
+
+const contenidoProductosServidor = (id, imagen, nombre, precio) => {
+    const contenido = `
+        <div class="productos__producto">
+            <div class="productos__imagen" style="background: url('${imagen}') center / 100% 100% no-repeat;" tabindex="0"></div>
+            <p class="productos__nombre parrafo" tabindex="0">${nombre}</p>
+            <p class="productos__precio parrafo" tabindex="0">${precio}</p>
+            <a class="productos__link link" href="./ventanas/detalles.html?id=${id}" title="Ver más detalles" tabindex="0">Ver
+                Producto</a>
+        </div>
+    `;
+    return contenido;
+}
+
+const crearListaProductosSimilares = () => {
+    const productosContainer = document.querySelector("[data-productos]");
+
+
+}
+
+productServices.listaProductos()
+    .then((productos) => {
+
+        for (let i = 0; i < productos.length; i++) {
+            const productoAleatorio = generarProductoAleatorio(productos);
+            if (!listaDesordenada.includes(productoAleatorio) && !listaDesordenada.includes(productos[i])) {
+                listaDesordenada.push(productoAleatorio);
+            }
+        }
+        /* const product = generarProductoAleatorio(productos);
+        console.log(productos); */
+        //console.log(listaDesordenada[0])
+        //console.log(listaDesordenada[0])
+        for(let i = 0; i < listaDesordenada.length; i++) {
+            for (let j = 0; j < productos.length; j++) {
+                console.log(listaDesordenada[i][j]);
+            }
+        }
+        /* listaDesordenada.forEach((item , i) => {
+            console.log(item[i].categoria);
+        }); */
+        /* listaDesordenada.forEach((item) => {
+            console.log(item)
+        }); */
+        //console.log(listaDesordenada[0][0].imagen);
+    })
+    .catch((error) => console.log(error));
 
 obtenerDetallesProducto();
+/* console.log(listaDesordenada); */
