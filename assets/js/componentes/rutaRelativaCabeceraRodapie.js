81580076905ea1731d933dcb358ed275301c9211
Eduardo <eduardoreyeshernandez98@gmail.com>
2022-05-12 21:49:30 -0500

    Agregando condición para ruta raíz sin index.html

--- assets/js/componentes/rutaRelativaCabeceraRodapie.js
+++ assets/js/componentes/rutaRelativaCabeceraRodapie.js
@@ -1,11 +1,11 @@
 export const rutaRelativaCabeceraRodapie = () => {
     const rutaActual = window.location.href;
-    /* const directorioRaiz = "./" */
     const archivoIndex = "index.html";
+    const rutaRaizOnline = "";
     const directorioVentanas = "/ventanas/";
     const directorioMensajes = "/ventanas/mensajes/";
     const listaRutas = ["./", "../", "../../"];
-    if (rutaActual.includes(archivoIndex)) {
+    if (rutaActual.includes(archivoIndex) || rutaActual.includes(rutaRaizOnline) && !rutaActual.includes(directorioVentanas)) {
         return listaRutas[0];
     } else if (rutaActual.includes(directorioVentanas) && !rutaActual.includes(directorioMensajes)) {
         return listaRutas[1];
