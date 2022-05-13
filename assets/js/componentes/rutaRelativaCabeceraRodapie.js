d5ea77d71f0cea0f77dea2584d15d5c2a5fa6273
Eduardo <eduardoreyeshernandez98@gmail.com>
2022-05-13 13:36:45 -0500

    Separando condición

--- assets/js/componentes/rutaRelativaCabeceraRodapie.js
+++ assets/js/componentes/rutaRelativaCabeceraRodapie.js
@@ -5,7 +5,7 @@ export const rutaRelativaCabeceraRodapie = () => {
     const directorioVentanas = "/ventanas/";
     const directorioMensajes = "/ventanas/mensajes/";
     const listaRutas = ["./", "../", "../../"];
-    if (rutaActual.includes(archivoIndex) || rutaActual.includes(rutaRaizOnline) && !rutaActual.includes(directorioVentanas)) {
+    if ((rutaActual.includes(archivoIndex) || rutaActual.includes(rutaRaizOnline)) && !rutaActual.includes(directorioVentanas)) {
         return listaRutas[0];
     } else if (rutaActual.includes(directorioVentanas) && !rutaActual.includes(directorioMensajes)) {
         return listaRutas[1];
