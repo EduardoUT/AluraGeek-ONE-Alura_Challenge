e223bd58f5bd443255b3dbe181d688f9b8291821
Eduardo Reyes Hernández <mcorey316@gmail.com>
2023-08-30 18:10:54 -0600

    Eliminando console

--- assets/js/controllers/productos-index-controller.js
+++ assets/js/controllers/productos-index-controller.js
@@ -69,7 +69,6 @@ const crearListaProductos = (productos, categoriaUnica, seccion) => {
             } else {
                 const contenidoProductosServidor = exhibirProductosServidor(id, imagen, nombre, precio);
                 listaProductos.innerHTML += contenidoProductosServidor;
-                console.log(listaProductos);
             }
             contadorProductos++;
             filtrarProductosExcedentes(listaProductos, contadorProductos);
