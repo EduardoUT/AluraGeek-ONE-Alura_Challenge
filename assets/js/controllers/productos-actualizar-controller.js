bd43057f6e0c3ce2281a261b559748739db0f041
Eduardo Reyes Hernández <mcorey316@gmail.com>
2023-09-01 17:05:14 -0600

    Agregando console de prueba

--- assets/js/controllers/productos-actualizar-controller.js
+++ assets/js/controllers/productos-actualizar-controller.js
@@ -17,6 +17,7 @@ const obtenerInformacion = async () => {
 
     try {
         const productoDetalles = await productServices.detalleProducto({id});
+        console.log(productoDetalles);
         const existenValores = (productoDetalles.imagen && productoDetalles.nombre &&
             productoDetalles.precio && productoDetalles.categoria && productoDetalles.desc);
 
