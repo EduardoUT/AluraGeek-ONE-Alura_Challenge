542d119e181c70a38abf78a7ec96834120372279
Eduardo Reyes Hernández <mcorey316@gmail.com>
2023-09-01 16:29:45 -0600

    Actualizando endpoint de usuarios

--- assets/js/service/user-service.js
+++ assets/js/service/user-service.js
@@ -1,4 +1,5 @@
-const listaUsuario = () => fetch("https://engaged-shiner-37.hasura.app/api/rest/usuario").then((respuesta) => respuesta.json());
+const listaUsuario = () => fetch("https://engaged-shiner-37.hasura.app/api/rest/consultarusuarios")
+    .then((respuesta) => respuesta.json());
 
 export const userServices = {
     listaUsuario,
