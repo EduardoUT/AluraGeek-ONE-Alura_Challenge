9013fcf3bca73cb5c67a4e7ce9b4558b5534c863
Eduardo Reyes Hernández <mcorey316@gmail.com>
2023-09-02 20:07:50 -0600

    Refactorizando código

--- assets/js/controllers/productos-categoria-controller.js
+++ assets/js/controllers/productos-categoria-controller.js
@@ -1,29 +1,29 @@
-import { productServices } from "../service/product-service.js";
+import { productServices, obtenerDatoArrayProducto } from "../service/product-service.js";
 
 const titleWindow = document.querySelector("title");
 const categoriaTitulo = document.querySelector("[data-categoria-titulo]");
 const listaProductos = document.querySelector("[data-categoria-productos]");
 
-const obtenerProducto = async (filtroCategoria) => {
-    filtroCategoria.forEach(({ id, imagen, nombre, precio }) => {
-        const rangoId = (id <= 18);
+const obtenerProducto = async (datoArrayProducto) => {
+    datoArrayProducto.forEach(({ id_producto, nombre_producto, precio_producto, imagen_producto }) => {
+        const rangoId = (id_producto <= 18);
         if (rangoId) {
             const contenidoLocal = `
                 <div class="productos__producto" style="display: flex; flex-direction: column; width: inherit;">
-                    <div class="productos__imagen" style="background: url('../assets/img/productos/${imagen}') center / 100% 100% no-repeat;" tabindex="0"></div>
-                    <p class="productos__nombre parrafo" tabindex="0">${nombre}</p>
-                    <p class="productos__precio parrafo" tabindex="0">${precio}</p>
-                    <a class="productos__link link" href="./productos_detalles.html?id=${id}" title="Ver más detalles" tabindex="0">Ver Producto</a>
+                    <div class="productos__imagen" style="background: url('../assets/img/productos/${imagen_producto}') center / 100% 100% no-repeat;" tabindex="0"></div>
+                    <p class="productos__nombre parrafo" tabindex="0">${nombre_producto}</p>
+                    <p class="productos__precio parrafo" tabindex="0">${precio_producto}</p>
+                    <a class="productos__link link" href="./productos_detalles.html?id=${id_producto}" title="Ver más detalles" tabindex="0">Ver Producto</a>
                 </div>
             `;
             listaProductos.innerHTML += contenidoLocal;
         } else {
             const contenidoServidor = `
                 <div class="productos__producto" style="display: flex; flex-direction: column; width: inherit;">
-                    <div class="productos__imagen" style="background: url('${imagen}') center / 100% 100% no-repeat;" tabindex="0"></div>
-                    <p class="productos__nombre parrafo" tabindex="0">${nombre}</p>
-                    <p class="productos__precio parrafo" tabindex="0">${precio}</p>
-                    <a class="productos__link link" href="./productos_detalles.html?id=${id}" title="Ver más detalles" tabindex="0">Ver Producto</a>
+                    <div class="productos__imagen" style="background: url('${imagen_producto}') center / 100% 100% no-repeat;" tabindex="0"></div>
+                    <p class="productos__nombre parrafo" tabindex="0">${nombre_producto}</p>
+                    <p class="productos__precio parrafo" tabindex="0">${precio_producto}</p>
+                    <a class="productos__link link" href="./productos_detalles.html?id=${id_producto}" title="Ver más detalles" tabindex="0">Ver Producto</a>
                 </div>
             `;
             listaProductos.innerHTML += contenidoServidor;
@@ -42,7 +42,8 @@ const obtenerCategoria = async () => {
     categoriaTitulo.textContent = categoria;
     try {
         const filtroCategoria = await productServices.detalleCategoria(categoria);
-        obtenerProducto(filtroCategoria);
+        const datoArrayProducto = obtenerDatoArrayProducto(filtroCategoria);
+        obtenerProducto(datoArrayProducto);
     } catch (error) {
         console.log(error);
     }
